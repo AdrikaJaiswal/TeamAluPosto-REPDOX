2229006684831882ad65a4734ee1ba9c41487771
shyalonaghosh-arch <shyalonaghosh@gmail.com>
2026-06-21 05:02:12 +0530

    fix model path casing

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/user");
 const AadhaarVerification = require("../models/AadhaarVerification");
 
 // Mock DigiLocker - simulates aadhaar verification
